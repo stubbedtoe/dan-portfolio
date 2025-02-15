3086ab7092eab8a507ff8ee198a9f4589e48ce43
Andrew Healy <werdnah19@gmail.com>
2025-02-15 17:50:51 +0000

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+import AstroPWA from '@vite-pwa/astro';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [AstroPWA()],
+});
