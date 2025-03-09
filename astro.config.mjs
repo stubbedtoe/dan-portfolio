2b730f40ad0b6b1f360c113dff1e66f5b99fc814
Andrew Healy <werdnah19@gmail.com>
2025-03-09 12:19:36 +0000

    get images working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from 'astro/config';
+import { imageService } from '@unpic/astro/service';
 import AstroPWA from '@vite-pwa/astro';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [AstroPWA()],
+    image: {
+        service: imageService(),
+        // This can usually be auto-detected
+        fallbackService: 'sharp',
+        placeholder: 'blurhash',
+        // This is the default
+        layout: 'constrained',
+    },
 });
