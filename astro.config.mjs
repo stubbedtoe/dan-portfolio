7e883b31f68eb700c1d28eb7ee3b440c1cff12bc
Andrew Healy <werdnah19@gmail.com>
2025-03-14 18:16:13 +0000

    constrain image and add bundle analyser

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
-import { defineConfig } from 'astro/config';
-import AstroPWA from '@vite-pwa/astro';
+import { defineConfig } from "astro/config";
+import AstroPWA from "@vite-pwa/astro";
+import { visualizer } from "rollup-plugin-visualizer";
 
-import playformCompress from '@playform/compress';
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [AstroPWA(), playformCompress()],
-    experimental: {
-        responsiveImages: true,
-    },
+  integrations: [AstroPWA(), playformCompress()],
+  experimental: {
+    responsiveImages: true,
+  },
+  vite: {
+    plugins: [
+      visualizer({
+        emitFile: true,
+        filename: "stats.html",
+      }),
+    ],
+  },
 });
