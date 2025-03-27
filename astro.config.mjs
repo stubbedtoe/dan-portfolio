dd92e171b7af1378a1f4c5222bf04516d1adb7b5
Andrew Healy <werdnah19@gmail.com>
2025-03-27 20:36:55 +0000

    use dm sans and create og images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,30 @@ import { defineConfig } from 'astro/config';
 import AstroPWA from '@vite-pwa/astro';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import compressor from 'astro-compressor';
+import opengraphImages, { presets } from 'astro-opengraph-images';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [
         AstroPWA(),
-        (await import('@playform/compress')).default({
-            Image: false,
-        }),
         (await import('@playform/inline')).default(),
         tailwindcss(),
         sitemap(),
+        opengraphImages({
+            options: {
+                fonts: [
+                    {
+                        name: 'DM Sans',
+                        weight: 400,
+                        style: 'normal',
+                        data: fs.readFileSync('node_modules/@fontsource/dm-sans/files/dm-sans-latin-400-normal.woff'),
+                    },
+                ],
+            },
+            render: presets.blackAndWhite,
+        }),
+        compressor(),
     ],
 
     experimental: {
