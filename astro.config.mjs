f5ea12d69dfcf4a4e267e23d49b52a1d4f013858
Andrew Healy <werdnah19@gmail.com>
2026-02-05 14:04:14 +0000

    update astro and add doctype

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,32 @@
-import { defineConfig } from 'astro/config';
-import AstroPWA from '@vite-pwa/astro';
-import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
-import compressor from 'astro-compressor';
+import { defineConfig } from "astro/config";
+import AstroPWA from "@vite-pwa/astro";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [AstroPWA(), (await import('@playform/inline')).default(), tailwindcss(), sitemap(), compressor()],
+  integrations: [
+    AstroPWA(),
+    (await import("@playform/inline")).default(),
+    tailwindcss(),
+    sitemap(),
+    compressor(),
+  ],
 
-    experimental: {
-        responsiveImages: true,
-        svg: true,
-    },
+  // experimental: {
+  //     responsiveImages: true,
+  //     svg: true,
+  // },
 
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
-    site: 'https://danmurrayartist.com',
+  site: "https://danmurrayartist.com",
 
-    prefetch: {
-        prefetchAll: true,
-        defaultStrategy: 'viewport',
-    },
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "viewport",
+  },
 });
