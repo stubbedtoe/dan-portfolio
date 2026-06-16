1a25d3dbff991e851702c108e28fa4f4fcc4ac76
Andrew Healy <werdnah19@gmail.com>
2026-06-16 18:17:55 +0100

    use postcss instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,13 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import compressor from 'astro-compressor';
 
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [(await import('@playform/inline')).default(), sitemap(), compressor()],
-  site: 'https://danmurrayartist.com',
+    integrations: [(await import('@playform/inline')).default(), sitemap(), compressor()],
+    site: 'https://danmurrayartist.com',
 
-  prefetch: {
-      prefetchAll: true,
-      defaultStrategy: 'viewport',
-  },
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    prefetch: {
+        prefetchAll: true,
+        defaultStrategy: 'viewport',
+    },
 });
